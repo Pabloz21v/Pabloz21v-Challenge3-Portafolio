da5019852d5b8f652f50dacfe8acd8523579a28c
Pablo Sebastian Acevedo <pabloz21v@gmail.com>
2023-04-26 08:30:36 -0300

    actualizacion portafolio

---
+++ flecha.js
@@ -0,0 +1,23 @@
+
+const perro =()=>({})
+const gato =()=>{return{};}
+const loro =()=>(({}))
+const pato =()=>{}
+const ver =(0 && "hola")
+console.log(ver)
+console.log("hola" && 1)
+console.log(0 || "hola")
+console.log("hola" || 1)
+
+
+console.log(perro());
+console.log(gato())
+console.log(loro())
+console.log(pato())
+
+//*asdsad*/
+// ()=>({})
+// ()=>{return{};}
+// ()=>(({}))
+// ()=>{}
+//const array = ["()=>({})","()=>{return{};}","()=>(({}))","()=>{}"]
